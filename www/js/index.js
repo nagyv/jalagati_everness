277c085dfb9066f736bc8904605444b44a4c1e4c
Viktor Nagy <viktor.nagy@gmail.com>
2014-07-02 22:40:51 +0200

    push notifications added

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,98 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var PUSH_SENDER_ID = "";
+
+var pushNotification;
+
+var pushHandler = {
+    successHandler: function(result) {
+        alert('result = ' + result);
+    },
+    errorHandler: function(error) {
+        alert('error = ' + error);
+    },
+    tokenHandler: function(result) {
+        // Your iOS push server needs to know the token before it can push to this device
+        // here is where you might want to send it the token for later use.
+        alert('device token = ' + result);
+    },
+    // iOS
+    onNotificationAPN: function (event) {
+        if ( event.alert )
+        {
+            navigator.notification.alert(event.alert);
+        }
+
+        if ( event.sound )
+        {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
+
+        if ( event.badge )
+        {
+            pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+        }
+    },
+
+    // Android
+    onNotificationGCM: function (e) {
+        $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+        switch( e.event )
+        {
+        case 'registered':
+            if ( e.regid.length > 0 )
+            {
+                $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+                // Your GCM push server needs to know the regID before it can push to this device
+                // here is where you might want to send it the regID for later use.
+                console.log("regID = " + e.regid);
+            }
+        break;
+
+        case 'message':
+            // if this flag is set, this notification happened while we were in the foreground.
+            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+            if ( e.foreground )
+            {
+                $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+                // if the notification contains a soundname, play it.
+                var my_media = new Media("/android_asset/www/"+e.soundname);
+                my_media.play();
+            }
+            else
+            {  // otherwise we were launched because the user touched a notification in the notification tray.
+                if ( e.coldstart )
+                {
+                    $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                }
+                else
+                {
+                    $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+                }
+            }
+
+            $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+            $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+        break;
+
+        case 'error':
+            $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+        break;
+
+        default:
+            $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+        break;
+      }
+    }
+
+};
+
 var app = {
+
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -33,7 +124,27 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        // app.receivedEvent('deviceready');
+        pushNotification = window.plugins.pushNotification;
+
+        if ( device.platform == 'android' || device.platform == 'Android' ) {
+            pushNotification.register(
+                pushHandler.successHandler,
+                pushHandler.errorHandler, {
+                    "senderID":PUSH_SENDER_ID,
+                    "ecb":"pushHandler.onNotificationGCM"
+                });
+        }
+        else {
+            pushNotification.register(
+                pushHandler.tokenHandler,
+                pushHandler.errorHandler, {
+                    "badge":"true",
+                    "sound":"true",
+                    "alert":"true",
+                    "ecb":"pushHandler.onNotificationAPN"
+                });
+        }
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
